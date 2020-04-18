431e10a77f6ee39efdb4dd27adf00cb4d5442687
harabasz <szymon.harabasz@gmail.com>
2020-04-18 21:22:37 +0200

    code duplication due to the workaround using JVM name removed by overriding mergeHelper and merge in subclass specified for Comparable parameters and moving all dependent functions back to the base class

--- src/main/kotlin/com/example/project/Heap.kt
+++ src/main/kotlin/com/example/project/Heap.kt
@@ -14,6 +14,31 @@ sealed class Heap<A> {
     abstract val size: Int
     abstract val isEmpty: Boolean
 
+    protected open fun mergeHelper(
+            left: Heap<A>,
+            head: A,
+            right: Heap<A>,
+            comparator: Result<Comparator<A>> =
+                    left.comparator.orElse { right.comparator }): Heap<A> = when {
+        left.rank < right.rank -> H(left.rank + 1, right, head, left, comparator)
+        else -> H(right.rank + 1, left, head, right, comparator)
+    }
+
+    open fun merge(first: Heap<A>, second: Heap<A>): Heap<A> = when (first) {
+        is Empty -> second
+        is H -> when (second) {
+            is Empty -> first
+            is H -> first.comparator
+                    .orElse { second.comparator }
+                    .getOrElse { throw IllegalStateException("no comparator found") }.let { comparator ->
+                        val smaller = if (comparator.compare(first.hd, second.hd) < 0) first else second
+                        val greater = if (comparator.compare(first.hd, second.hd) < 0) second else first
+                        val right = merge(smaller.rg, greater)
+                        mergeHelper(smaller.lt, smaller.hd, right, Result(comparator))
+                    }
+        }
+    }
+
     override fun toString(): String {
         val strLeft = left.map { "$it" }.getOrElse("F")
         val strHead = head.map { "$it" }.getOrElse("F")
@@ -36,6 +61,32 @@ sealed class Heap<A> {
         override val size: Int = 0
 
     }
+    operator fun plus(element: A): Heap<A> = merge(this, Heap(element, this.comparator))
+
+    private fun tail(): Result<Heap<A>> = when (this) {
+        is Heap.H -> Result(merge(lt, rg))
+        else -> Result.failure("tail called on empty heap")
+    }
+
+    fun get(index: Int): Result<A> = when (this) {
+        is Heap.H -> when (index) {
+            0 -> head
+            else -> this.tail().flatMap { it.get(index - 1) }
+        }
+        else -> Result.failure("element not found")
+    }
+
+    fun pop(): Option<Pair<A, Heap<A>>> = when (this) {
+        is Heap.H -> Option.Some(Pair(hd, merge(lt, rg)))
+        else -> Option.None as Option<Pair<A, Heap<A>>>
+    }
+
+    fun toList(): LinkedList<A> = foldLeft(LinkedList<A>()) { list ->
+        { element -> list.cons(element) }
+    }.reverse()
+
+    fun <B> foldLeft(identity: B, f: (B) -> (A) -> B): B =
+            unfold(this, identity, { it.pop() }, f)
 
     class EmptyComparable<A : Comparable<A>> : Empty<A>() {
         override fun toString(): String = "HC{E $comparator}"
@@ -79,6 +130,43 @@ sealed class Heap<A> {
             val strRight = right.map { "$it" }.getOrElse("F")
             return "HC{$rank $strLeft $strHead $strRight $comparator}"
         }
+
+        override fun mergeHelper(
+                left: Heap<A>,
+                head: A,
+                right: Heap<A>,
+                comparator: Result<Comparator<A>>): Heap<A> = when {
+            left.rank < right.rank -> HComparable(left.rank + 1, right, head, left)
+            else -> HComparable(right.rank + 1, left, head, right)
+        }
+
+        override fun merge(first: Heap<A>, second: Heap<A>): Heap<A> = when (first) {
+            is Empty -> second
+            is H -> when (second) {
+                is Empty -> first
+                is H -> first.comparator
+                        .orElse { second.comparator }
+                        .orElse { Result.failure("no comparator found") }.let { comparator ->
+                            when (comparator) {
+                                is Result.Success -> {
+                                    println("comparator is Success: $first, $second")
+                                    val smaller = if (comparator.value.compare(first.hd, second.hd) < 0) first else second
+                                    val greater = if (comparator.value.compare(first.hd, second.hd) < 0) second else first
+                                    val right = merge(smaller.rg, greater)
+                                    mergeHelper(smaller.lt, smaller.hd, right, comparator)
+                                }
+                                else -> {
+                                    println("comparator is Failure: $first, $second")
+                                    val smaller = if (first.hd < second.hd) first else second
+                                    val greater = if (first.hd < second.hd) second else first
+                                    val right = merge(smaller.rg, greater)
+                                    mergeHelper(smaller.lt, smaller.hd, right)
+                                }
+                            }
+                        }
+            }
+        }
+
     }
 
     companion object {
@@ -106,69 +194,7 @@ sealed class Heap<A> {
                         comparator)
 
 
-        private fun <A: Comparable<A>> mergeHelper(
-                left: Heap<A>,
-                head: A,
-                right: Heap<A>,
-                comparator: Result<Comparator<A>> =
-                        left.comparator.orElse { right.comparator }): Heap<A> = when {
-            left.rank < right.rank -> HComparable(left.rank + 1, right, head, left)
-            else -> HComparable(right.rank + 1, left, head, right)
-        }
 
-        @JvmName("mergeComparator")
-        private fun <A> mergeHelper(
-                left: Heap<A>,
-                head: A,
-                right: Heap<A>,
-                comparator: Result<Comparator<A>> =
-                        left.comparator.orElse { right.comparator }): Heap<A> = when {
-            left.rank < right.rank -> H(left.rank + 1, right, head, left, comparator)
-            else -> H(right.rank + 1, left, head, right, comparator)
-        }
-
-        fun <A : Comparable<A>> merge(first: Heap<A>, second: Heap<A>): Heap<A> = when (first) {
-            is Empty -> second
-            is H -> when (second) {
-                is Empty -> first
-                is H -> first.comparator
-                        .orElse { second.comparator }
-                        .orElse { Result.failure("no comparator found") }.let { comparator ->
-                            when (comparator) {
-                                is Result.Success -> {
-                                    println("comparator is Success: $first, $second")
-                                    val smaller = if (comparator.value.compare(first.hd, second.hd) < 0) first else second
-                                    val greater = if (comparator.value.compare(first.hd, second.hd) < 0) second else first
-                                    val right = merge(smaller.rg, greater)
-                                    mergeHelper(smaller.lt, smaller.hd, right, comparator)
-                                }
-                                else -> {
-                                    println("comparator is Failure: $first, $second")
-                                    val smaller = if (first.hd < second.hd) first else second
-                                    val greater = if (first.hd < second.hd) second else first
-                                    val right = merge(smaller.rg, greater)
-                                    mergeHelper(smaller.lt, smaller.hd, right)
-                                }
-                            }
-                        }
-            }
-        }
-
-        @JvmName("mergeComparator")
-        fun <A> merge(first: Heap<A>, second: Heap<A>): Heap<A> = when (first) {
-            is Empty -> second
-            is H -> when (second) {
-                is Empty -> first
-                is H -> first.comparator
-                        .orElse { second.comparator }
-                        .getOrElse { throw IllegalStateException("no comparator found") }.let { comparator ->
-                            val smaller = if (comparator.compare(first.hd, second.hd) < 0) first else second
-                            val greater = if (comparator.compare(first.hd, second.hd) < 0) second else first
-                            val right = merge(smaller.rg, greater)
-                            mergeHelper(smaller.lt, smaller.hd, right, Result(comparator))
-                        }
-            }
-        }
 
     }
 }
@@ -182,59 +208,5 @@ fun <A, B, S> unfold (z: S, identity: B, f: (S) -> Option<Pair<A, S>>, g: (B) ->
     }
     return go(z, identity)
 }
-
-@JvmName("plusComparator")
-operator fun <A> Heap<A>.plus(element: A): Heap<A> = Heap.merge(this, Heap(element, this.comparator))
-operator fun <A: Comparable<A>> Heap<A>.plus(element: A): Heap<A> = Heap.merge(this, Heap(element, this.comparator))
-
-@JvmName("tailComparator")
-internal fun <A> Heap.H<A>.tail(): Result<Heap<A>> = when (this) {
-    is Heap.H -> Result(Heap.merge(lt, rg))
-    else -> Result.failure("tail called on empty heap")
-}
-internal fun <A: Comparable<A>> Heap.H<A>.tail(): Result<Heap<A>> = when (this) {
-    is Heap.H -> Result(Heap.merge(lt, rg))
-    else -> Result.failure("tail called on empty heap")
-}
-
-@JvmName("getComparator")
-fun <A> Heap<A>.get(index: Int): Result<A> = when (this) {
-    is Heap.H -> when (index) {
-        0 -> head
-        else -> this.tail().flatMap { it.get(index - 1) }
-    }
-    else -> Result.failure("element not found")
-}
-fun <A: Comparable<A>> Heap<A>.get(index: Int): Result<A> = when (this) {
-    is Heap.H -> when (index) {
-        0 -> head
-        else -> this.tail().flatMap { it.get(index - 1) }
-    }
-    else -> Result.failure("element not found")
-}
-
-@JvmName("popComparator")
-fun <A> Heap<A>.pop(): Option<Pair<A, Heap<A>>> = when (this) {
-    is Heap.H -> Option.Some(Pair(hd, Heap.merge(lt, rg)))
-    else -> Option.None as Option<Pair<A, Heap<A>>>
-}
-fun <A: Comparable<A>> Heap<A>.pop(): Option<Pair<A, Heap<A>>> = when (this) {
-    is Heap.H -> Option.Some(Pair(hd, Heap.merge(lt, rg)))
-    else -> Option.None as Option<Pair<A, Heap<A>>>
-}
-
-@JvmName("toListComparator")
-fun <A> Heap<A>.toList(): LinkedList<A> = foldLeft(LinkedList<A>()) { list ->
-    { element -> list.cons(element) }
-}.reverse()
-fun <A: Comparable<A>> Heap<A>.toList(): LinkedList<A> = foldLeft(LinkedList<A>()) { list ->
-    { element -> list.cons(element) }
-}.reverse()
-
-@JvmName("foldLeftComparator")
-fun <A, B> Heap<A>.foldLeft(identity: B, f: (B) -> (A) -> B): B =
-        unfold(this, identity, { it.pop() }, f)
-fun <A: Comparable<A>, B> Heap<A>.foldLeft(identity: B, f: (B) -> (A) -> B): B =
-        unfold(this, identity, { it.pop() }, f)
 
 
